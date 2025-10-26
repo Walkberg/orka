48d3cc5ed0744e9a978593d9b00a47a04c8efb80
Walkberg <samuel.bergerot@gmail.com>
2025-10-26 17:07:37 +0100

    move

--- apps/react-example/vite.config.ts
+++ apps/react-example/vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@orka-react": path.resolve(__dirname, "../../libs/orka-react/src"),
-    },
+    alias: {},
   },
 });
--- libs/orka-react/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@/components/SignIn";
-export * from "@/components/SignUp";
-export * from "@/components/UserProfile";
-export * from "@/components/SignedIn";
-export * from "@/components/SignedOut";
-export * from "@/providers/OrkaProvider";
